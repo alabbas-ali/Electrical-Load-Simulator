6baa3e16b54b1d909a98cff2b34df4e764ce8ad9
abass ali <weeamha@gmail.com>
2017-11-17 00:40:30 +0100

    change view template from jsp to Thymeleaf

--- src/main/java/his/loadprofile/config/WebApplication.java
+++ src/main/java/his/loadprofile/config/WebApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "his.loadprofile.controller")
@@ -15,14 +17,39 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class WebApplication extends SpringBootServletInitializer {
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
-	{
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(WebApplication.class);
 	}
 
-	public static void main(String[] args) throws Exception 
-	{
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WebApplication.class, args);
 	}
 
+	@Bean
+	public ServletContextTemplateResolver defaultTemplateResolver() {
+		ServletContextTemplateResolver yourTemplateResolver = new ServletContextTemplateResolver();
+		yourTemplateResolver.setPrefix("/WEB-INF/templates/");
+		yourTemplateResolver.setSuffix(".html");
+		yourTemplateResolver.setTemplateMode("HTML5");
+		yourTemplateResolver.setCharacterEncoding("UTF-8");
+
+		return yourTemplateResolver;
+	}
+	
+//	@Bean
+//	public SpringTemplateEngine springTemplateEngine(TemplateResolver templateResolver) {
+//		SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+//		springTemplateEngine.addTemplateResolver(templateResolver);
+//
+//		return springTemplateEngine;
+//	}
+//	
+//	@Bean
+//	public ThymeleafViewResolver thymeleafViewResolver(SpringTemplateEngine templateEngine) {
+//		ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+//		thymeleafViewResolver.setTemplateEngine(templateEngine);
+//		thymeleafViewResolver.setOrder(1);
+//		thymeleafViewResolver.setViewNames(new String [] {"thymeleaf/*"});
+//		return thymeleafViewResolver;
+//	}
 }
