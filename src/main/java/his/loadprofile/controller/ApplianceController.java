5b7c360e72017dbf8c9b8fec4395d77428b6a382
abass ali <weeamha@gmail.com>
2017-11-19 14:27:34 +0100

    seperte apliances and availabilities

--- src/main/java/his/loadprofile/controller/ApplianceController.java
+++ src/main/java/his/loadprofile/controller/ApplianceController.java
@@ -1,24 +1,42 @@
 package his.loadprofile.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import his.loadprofile.model.Appliance;
+import his.loadprofile.repo.ApplianceRepository;
 
 @Controller
 public class ApplianceController {
 	
+	@Autowired
+	private ApplianceRepository applianceRepository;
+	
+	@RequestMapping("/appliance")
+	public String index(Map<String, Object> model) 
+	{
+		List<Appliance> appliances = applianceRepository.findAll();
+		model.put("appliances", appliances);
+		return "appliance/index";
+	}
+	
 	@RequestMapping("/appliance/details/{id}")
 	public String details(Map<String, Object> model) 
 	{
-		        
+		// get the Appliance by id ang pass it in model
+		
         return "appliance/details";
 	}
 	
 	@RequestMapping("/appliance/edit/{id}")
 	public String edit(Map<String, Object> model) 
 	{
-		        
+		// get the data and save or update Appliance
+		
         return "appliance/edit";
 	}
 
--- src/main/java/his/loadprofile/controller/AvailabilityController.java
+++ src/main/java/his/loadprofile/controller/AvailabilityController.java
@@ -1,12 +1,24 @@
 package his.loadprofile.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import his.loadprofile.model.Availability;
+
 @Controller
 public class AvailabilityController {
+	
+	@RequestMapping("/availability")
+	public String index(Map<String, Object> model) 
+	{
+		List<Availability> availabilities = new ArrayList<Availability>();
+		model.put("availabilities", availabilities);
+		return "availability/index";
+	}
 	
 	@RequestMapping("/availability/details/{id}")
 	public String details(Map<String, Object> model) 
