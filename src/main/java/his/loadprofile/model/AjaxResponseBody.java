94156e795268b057929f298756982e7e5948fb71
weeam <weeamha@gmail.com>
2017-11-21 02:38:13 +0100

    Add the Ajax Response Body BOJO

---
+++ src/main/java/his/loadprofile/model/AjaxResponseBody.java
@@ -0,0 +1,34 @@
+package his.loadprofile.model;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+public class AjaxResponseBody {
+	
+	@JsonView(Views.Public.class)
+	String msg;
+
+	@JsonView(Views.Public.class)
+	String code;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+}
+
+class Views {
+    public static class Public {}
+}
