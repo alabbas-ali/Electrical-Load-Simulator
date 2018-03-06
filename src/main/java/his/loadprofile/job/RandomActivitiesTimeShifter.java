4f22e9b60f5f4b8245f26e21747af8299496afea
abass ali <weeamha@gmail.com>
2018-03-07 00:42:25 +0100

    merge wwith mean_stdev

--- src/main/java/his/loadprofile/job/RandomActivitiesTimeShifter.java
+++ src/main/java/his/loadprofile/job/RandomActivitiesTimeShifter.java
@@ -10,51 +10,49 @@ import his.loadprofile.core.ActivityType;
 import his.loadprofile.model.Activity;
 import his.loadprofile.model.Availability;
 
-public class RandomActivitiesTimeShifter implements ActivitiesTimeShifter{
-	
-	static final long ONE_MINUTE_IN_MILLIS = 60000;//millisecs
-	
+public class RandomActivitiesTimeShifter implements ActivitiesTimeShifter {
+
+	static final long ONE_MINUTE_IN_MILLIS = 60000;// millisecs
+
 	private double mean;
 	private double standDev;
-	
-	
+
 	@Override
 	public List<Availability> shift(List<Availability> availabilitiesList) {
-		
+
 		Random r = new Random();
 		Calendar cal = new GregorianCalendar();
 		long t;
 		Date afterAddingTenMins;
 		List<Activity> activityList;
-		
+
 		for (Availability availability : availabilitiesList) {
-			
+
 			activityList = availability.getActivities();
-			
-			for (int i=0;i< activityList.size();i++) {
+
+			for (int i = 0; i < activityList.size(); i++) {
 				this.getActivityMeanValue(activityList.get(i).getType());
 				double normalRandomNumber = r.nextGaussian() * this.standDev + this.mean;
-				
+
 				cal.setTime(activityList.get(i).getStart());
 				t = cal.getTimeInMillis();
 				afterAddingTenMins = new Date((long) (t + (normalRandomNumber * ONE_MINUTE_IN_MILLIS)));
 				activityList.get(i).setStart(afterAddingTenMins);
-				
+
 				cal.setTime(activityList.get(i).getEnd());
 				t = cal.getTimeInMillis();
 				afterAddingTenMins = new Date((long) (t + (normalRandomNumber * ONE_MINUTE_IN_MILLIS)));
 				activityList.get(i).setEnd(afterAddingTenMins);
 			}
-			
+
 			availability.setActivities(activityList);
 		}
-		
-			
+
 		return availabilitiesList;
 	}
-	
+
 	private void getActivityMeanValue(ActivityType type) {
-		
+
 		switch (type) {
 		case ACTIVITY_WAKUP:
 			this.mean = 7;
@@ -101,7 +99,7 @@ public class RandomActivitiesTimeShifter implements ActivitiesTimeShifter{
 			this.standDev = 1.5;
 			break;
 		case ACTIVITY_WASHCLOTHES:
-			this.mean = 19.45; 
+			this.mean = 19.45;
 			this.standDev = 1.5;
 			break;
 		case ACTIVITY_IRONING:
@@ -136,10 +134,8 @@ public class RandomActivitiesTimeShifter implements ActivitiesTimeShifter{
 			this.mean = 20.45;
 			this.standDev = 1.5;
 			break;
-			default:
-				break;
 		}
-		
+
 	}
 
 }
